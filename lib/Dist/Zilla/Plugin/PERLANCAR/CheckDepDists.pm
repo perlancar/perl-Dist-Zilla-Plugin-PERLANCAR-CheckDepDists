f16fbf585e63303f35f17f1b98767d1822c9653c
perlancar (@pc-home) <perlancar@gmail.com>
2015-07-28 10:36:52 +0700

    Search for .tag-requires-dist-<DISTNAME> tag files

--- lib/Dist/Zilla/Plugin/PERLANCAR/CheckDepDists.pm
+++ lib/Dist/Zilla/Plugin/PERLANCAR/CheckDepDists.pm
@@ -13,6 +13,7 @@ with (
 );
 
 use App::lcpan::Call qw(call_lcpan_script);
+use File::chdir;
 use Module::List qw(list_modules);
 use Sub::NoRepeat qw(norepeat);
 use Term::ANSIColor;
@@ -24,6 +25,8 @@ sub after_build {
     no strict 'refs';
 
     my $self = shift;
+
+    my $color = "bold cyan";
 
     norepeat(
         key => __PACKAGE__ . ' ' . $self->zilla->name,
@@ -40,7 +43,20 @@ sub after_build {
                 my $dists = \@{"$mod\::" . ($lump ? "LUMPED_DISTS" : "PACKED_DISTS")};
                 if ($self->zilla->name ~~ @$dists) {
                     my $dist = $mod; $dist =~ s/::/-/g;
-                    say colored(["bold cyan"], "This distribution also needs to be rebuilt: $dist");
+                    say colored([$color], "This distribution also needs to be rebuilt: $dist");
+                }
+            }
+
+            # XXX some configurability wrt repos directory
+            $self->log_debug(["Finding all repos which requires our dist ..."]);
+            {
+                local $CWD = "..";
+                my $tag_filename = ".tag-requires-dist-" . $self->zilla->name;
+                for my $repo (grep {-d} <*>) {
+                    local $CWD = $repo;
+                    if (-f $tag_filename) {
+                        say colored([$color], "This repo also needs to be rebuilt: $repo");
+                    }
                 }
             }
         },
@@ -62,28 +78,35 @@ In C<dist.ini>:
 
 =head1 DESCRIPTION
 
-This plugin checks for dists that depend on the dist you're building. Currently
-what it does:
+This plugin notifies you, in the after_build phase, of dists that might need to
+be rebuild too, because those dists depend on the dist you're building.
+Currently what it does:
 
 =over
 
 =item *
 
-In the after_build phase, search your local installation for all lump dists (via
-searching all modules whose name ends with C<::Lumped>). Inside each of these
-modules, there is a C<@LUMPED_DISTS> array which lists all the dists that the
-lump dist includes. When the current dist you're building is listed in
-C<@LUMPED_DISTS>, the plugin will issue a notification that you will also need
-to rebuild the associated lump dist.
+Search your local installation for all lump dists (via searching all modules
+whose name ends with C<::Lumped>). Inside each of these modules, there is a
+C<@LUMPED_DISTS> array which lists all the dists that the lump dist includes.
+When the current dist you're building is listed in C<@LUMPED_DISTS>, the plugin
+will issue a notification that you will also need to rebuild the associated lump
+dist.
 
 =item *
 
-In the after_build phase, search your local installation for all packed dists
-(via searching all modules whose name ends with C<::Packed>, C<::Fatpacked>,
-C<::DataPacked>). Inside each of these modules, there is a C<@PACKED_DISTS>
-array which lists all the dists that the packed dist includes. When the current
-dist you're building is listed in C<@PACKED_DISTS>, the plugin will issue a
-notification that you will also need to rebuild the associated packed dist.
+Search your local installation for all packed dists (via searching all modules
+whose name ends with C<::Packed>, C<::Fatpacked>, C<::DataPacked>). Inside each
+of these modules, there is a C<@PACKED_DISTS> array which lists all the dists
+that the packed dist includes. When the current dist you're building is listed
+in C<@PACKED_DISTS>, the plugin will issue a notification that you will also
+need to rebuild the associated packed dist.
+
+=item *
+
+Search C<../> (XXX probably should be configurable) for all repos (dirs) that
+contains tag file C<.tag-requires-dist-DISTNAME> where I<DISTNAME> is the
+current distribution's name.
 
 =back
 
